6456ab949d9ee705e4b6c5ad5ae594266cccf1b0
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-11-09 11:40:40 -0300

    feat: add base route, 404 and 500

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
-import "dotenv/config";
+import "dotenv/config"
 
-import express from "express";
-import cors from "cors";
-import { router } from "@src/routes";
+import express, { Errback, NextFunction, Request, Response } from "express"
+import cors from "cors"
+import { router } from "@src/routes"
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.use(router);
+app.use(router)
 
-export { app };
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Page Not Found" })
+})
+
+app.use((err: Errback, req: Request, res: Response, next: NextFunction) => {
+  return res.status(500).json({ message: "Internal Error" })
+})
+
+export { app }
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
-import { Router } from "express";
+import { Router } from "express"
 
-const router = Router();
+const router = Router()
 
-export { router };
+router.get("/", (req, res) => {
+  res.json({ message: "Hello world!" })
+})
+
+export { router }
